ae032e891ef37e6ac5c0726d913b06e1a028f0e9
Cedrick James Felicitas <77721340+CJ-Felicitas@users.noreply.github.com>
2023-12-06 14:22:01 +0000

    navbar

--- index.php
+++ index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="grid.css">
 </head>
 <body>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
     <div class="container">
         <div class="row">
         <div class="col-md-12">
-        this is just a test
+        
         </div>
         </div>
     </div>
