9f7b427ea6d6436a4d34feeee44fa8a9b004d6a7
Cedrick James Felicitas <77721340+CJ-Felicitas@users.noreply.github.com>
2023-12-06 14:13:15 +0000

    test run

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-test push
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+        this is just a test
+        </div>
+        </div>
+    </div>
+</body>
+</html>
