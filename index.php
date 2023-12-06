d0c5ca9d68379b365fec814837077b2a262e2db7
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2023-12-07 00:27:08 +0800

    layout done

--- index.php
+++ index.php
@@ -1,25 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="grid.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="buttons.css">
+    <style>
+    
+    </style>
 </head>
+
 <body>
-<ul>
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
-    <div class="container">
-        <div class="row">
-        <div class="col-md-12">
-        
-        </div>
+    <div class="row">
+        <div class="col-md-12 head-style">
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mid">
+                <h1>COFFEE SHOP!</h1>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 50px">
+            <div class="col-md-12 mid">
+                <div class="col-md-5" id="">
+                    <div>
+                        <img src="images/coffee_one.png" alt="" width=100%>
+                    </div>
+                </div>
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-5" id="">
+                    <div>
+                        <img src="images/coffee_two.png" width=100% alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row space-top">
+                <div class="col-md-12">
+                    <div class="mid">
+                        <a href="order.php">  <button class="btn btn-primary"><h3>Order Now</h3></button></a>
+                    </div>
+                </div>
+        </div>
+    </div>
+<!-- about page
+ -->
+    <div class="container-extended" style="margin-top:40px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <center>
+                        <h1>About</h1>
+                    </center>
+                    <div class="about">
+
+                        Welcome to [Your Coffee Shop Name], where passion for coffee meets a cozy ambiance.
+
+                        At [Your Coffee Shop Name], we believe that every cup of coffee tells a story. Our journey began
+                        with a simple desire to create a space where coffee lovers could come together to savor
+                        exceptional brews in a warm and inviting atmosphere.
+
+                        What sets us apart is our commitment to quality. We source only the finest beans from around the
+                        world, ensuring that each cup is a celebration of flavor and craftsmanship. Our skilled baristas
+                        are dedicated to the art of coffee-making, meticulously preparing each beverage with precision
+                        and care.
+
+                        Step inside our welcoming haven, where the rich aroma of freshly ground beans mingles with the
+                        comforting hum of conversation. Our cozy seating and rustic decor provide the perfect backdrop
+                        for enjoying your favorite brew, whether you're catching up with friends, diving into a good
+                        book, or simply savoring a moment of solitude.
+
+                        Beyond our exceptional coffee, we take pride in offering a diverse menu of delightful treats.
+                        From artisanal pastries to wholesome sandwiches, our menu is crafted to complement the nuanced
+                        profiles of our coffee blends. We understand that every palate is unique, and our menu reflects
+                        our dedication to catering to a variety of tastes.
+
+                        [Your Coffee Shop Name] is more than just a place to grab a cup of coffee; it's a community. We
+                        value the connections forged over shared moments and conversations. Our friendly staff is here
+                        to make your experience memorable, whether you're a seasoned coffee connoisseur or a casual
+                        visitor seeking a comforting cup.
+
+                        Join us on a journey of taste and community. At [Your Coffee Shop Name], every cup is an
+                        invitation to savor the moment, and every visit is an opportunity to create new memories. We
+                        look forward to welcoming you to our home away from home.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 </body>
+
 </html>
---
+++ order.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="grid.css">
+    <link rel="stylesheet" href="buttons.css">
+</head>
+
+<body>
+    <div class="head-style">
+    </div>
+    <div class="container">
+        <!--  -->
+        <center>
+            <h1>Select Order</h1>
+        </center>
+        <div class="row">
+            <div class="col-md-4">
+                <div style="padding: 10px;">
+                    <div>
+                        <img src="images/matcha.png" alt="" width=100%>
+                    </div>
+                    <form action="addtocart.php" method="post">
+                        <input type="hidden" name="matcha">
+                        <br>
+                        <center> <input value="0" class="custom-form-control" type="number" id="quantity"
+                                name="quantity" min="1" max="5"></center>
+
+                </div>
+            </div>
+            <!--  -->
+            <div class="col-md-4">
+                <div style="padding: 10px;">
+                    <div>
+                        <img src="images/choco.png" alt="" width=100%>
+                    </div>
+
+                    <input type="hidden" name="choco">
+                    <br>
+                    <center> <input value="0" class="custom-form-control" type="number" id="quantity" name="quantity"
+                            min="1" max="5"></center>
+
+                </div>
+            </div>
+            <!--  -->
+            <div class="col-md-4">
+                <div style="padding: 10px;">
+                    <div>
+                        <img src="images/espresso.png" alt="" width=100%>
+                    </div>
+
+                    <input type="hidden" name="espresso">
+                    <br>
+                    <center> <input value="0" class="custom-form-control" type="number" id="quantity" name="quantity"
+                            min="1" max="5"></center>
+
+                </div>
+            </div>
+            <!--  -->
+        </div>
+
+        <div class="row">
+            <center> <button type="submit" class="btn btn-success">Checkout</button> </center>
+            </form>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
