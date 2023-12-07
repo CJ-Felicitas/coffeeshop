cb9f7f5dc2cd0b57ee29c7fa3d3047285306fc03
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2023-12-07 12:05:01 +0800

    add to prod script:

--- input.php
+++ input.php
@@ -25,26 +25,46 @@
         <div class="row">
             <div class="col-md-12 mid">
                 <div class="col-md-5">
-                    <input type="text" class="custom-form-control" placeholder="Name of the Product">
-                    <br>
-                    <input type="text" class="custom-form-control" placeholder="Price of the Product"><br>
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <input type="text" class="custom-form-control" name="productName"
+                            placeholder="Name of the Product">
+                        <br>
+                        <input type="text" class="custom-form-control" name="productPrice"
+                            placeholder="Price of the Product">
+                        <br>
+                        <input type="text" class="custom-form-control" name="productDescription"
+                            placeholder="Description of the Product">
+                        <br>
+                        <center>
+                            <button class="btn btn-success" type="submit" name="addProduct">Add Product</button>
+                        </center>
+                    </form>
 
-                    <input type="text" class="custom-form-control" placeholder="Description of the Product">
-                    <br>
+                    <?php
+                    // http post req
+                    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["addProduct"])) {
+
+                        $productName = $_POST["productName"];
+                        $productPrice = $_POST["productPrice"];
+                        $productDescription = $_POST["productDescription"];
+
+                        include 'dbconfig.php';
+
+                        $sql = "INSERT INTO products (product_name, product_price, product_description) VALUES ('$productName', $productPrice, '$productDescription')";
+
+                        if ($conn->query($sql) === TRUE) {
+                            header("Location: display.php?ProductAddedSuccessfully");
+                        } else {
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+
+                        $conn->close();
+                    }
+                    ?>
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-md-12 mid">
-                <div class="col-md-5">
-                    <center> <button class="btn btn-success">Add Product</button></center>
-
-                </div>
-            </div>
-        </div>
-
-    </div>
+    </div>  
 </body>
 
 </html>
