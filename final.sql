2a53c504484cf20fea58c98eb13415a76433907d
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2023-12-09 20:31:42 +0800

    change unicode

--- final.sql
+++ final.sql
@@ -1,32 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 09, 2023 at 12:22 PM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `coffeeshop`
---
-drop database if exists 'coffeeshop';
-create database coffeeshop;
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
+DROP DATABASE IF EXISTS coffeeshop;
+CREATE DATABASE coffeeshop;
 
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
@@ -37,22 +10,12 @@ CREATE TABLE IF NOT EXISTS `orders` (
   PRIMARY KEY (`id`),
   KEY `customer_id` (`customer_name`(250)),
   KEY `product_id` (`product_name`(250))
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `orders`
---
+);
 
 INSERT INTO `orders` (`id`, `customer_name`, `product_name`, `product_price`) VALUES
 (11, 'asd', 'lakjsdlaskjd', '999999.00'),
 (12, 'asd', 'isog na kape', '234234.00'),
 (13, 'cj', 'isog na kape', '234234.00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
@@ -61,19 +24,10 @@ CREATE TABLE IF NOT EXISTS `products` (
   `product_price` double DEFAULT NULL,
   `product_description` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `products`
---
+);
 
 INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_description`) VALUES
 (36, 'lakjsdlaskjd', 999999, 'sdkfsdkljf'),
 (35, 'isog na kape', 234234, 'skljfdslfk'),
 (34, 'Matcha', 1000000, 'alskdjalskdj'),
 (33, 'Caramel', 23432432, 'delicious');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
