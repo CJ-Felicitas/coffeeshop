90b5319bc8dd0c74c16fcc4865423a1ebde08f6f
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2023-12-09 20:26:49 +0800

    use db

--- final.sql
+++ final.sql
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 
 create database coffeeshop;
 -- --------------------------------------------------------
-
+use coffeeshop;
 --
 -- Table structure for table `orders`
 --
