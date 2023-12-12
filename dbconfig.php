135afeb65b495362eddc2501c6c5539a74b5d4ee
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2023-12-12 19:13:23 +0800

    test

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-// Database configuration
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -8,7 +8,7 @@ $database = "coffeeshop";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-// terminate connection if error
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
--- delete_product.php
+++ delete_product.php
@@ -4,14 +4,13 @@ include 'dbconfig.php';
 if (isset($_POST['delete_product'])) {
     $product_id = $_POST['product_id'];
 
-    // Sanitize the input
     $product_id = mysqli_real_escape_string($conn, $product_id);
 
-    // Perform deletion query
+
     $delete_query = "DELETE FROM products WHERE id = $product_id";
 
     if ($conn->query($delete_query) === TRUE) {
-        header("Location: display.php"); // Redirect back to display.php after deletion
+        header("Location: display.php"); 
         exit();
     } else {
         echo "Error deleting product: " . $conn->error;
